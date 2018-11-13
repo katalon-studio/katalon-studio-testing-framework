b550244caa453e8d875486526f0593633e1c07f3
Hai Minh Nguyen <nguyen.minh.hai@live.com>
2018-11-13 17:08:46 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+Katalon Built-In Keywords
+=========================
+
+## Contributing Guidelines
+
+The following files demonstrate how to contribute a new keyword:
+* `Include/scripts/groovy/com/kms/katalon/core/webui/keyword/WebUiBuiltInKeywords.groovy`
+* `Include/scripts/groovy/com/kms/katalon/core/webui/keyword/builtin/HelloWorldFromWebUiKeyword.groovy`
+* `Test Cases/HelloWorldFromWebUiTestCase`
+
+## Troubleshooting
+
+## License
+
+This source code is released under the LICENSE AGREEMENT FOR KATALON AUTOMATION FRAMEWORK.
---
+++ Test Suites/All Test Cases.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>All Test Cases</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <lastRun>2018-11-13T16:53:16</lastRun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>f9fbc537-78c2-40ed-aeae-dcf567db202b</testSuiteGuid>
+   <testCaseLink>
+      <guid>4d573128-d481-4502-b7eb-803ec768e987</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/HelloWorldFromWebUiTestCase</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
