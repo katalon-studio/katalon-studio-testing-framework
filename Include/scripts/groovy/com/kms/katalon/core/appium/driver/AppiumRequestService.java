3f5defc8c8686d2713f67b3f6ada36148829c704
HuyenNguyen <huyenthingocnguyen@kms-technology.com>
2020-12-22 11:05:13 +0700

    Add com.kms.katalon.appium

--- Include/scripts/groovy/com/kms/katalon/core/appium/driver/AppiumRequestService.java
+++ Include/scripts/groovy/com/kms/katalon/core/appium/driver/AppiumRequestService.java
@@ -1,19 +1,24 @@
 package com.kms.katalon.core.appium.driver;
 
 import java.io.IOException;
+import java.net.URISyntaxException;
+import java.security.GeneralSecurityException;
 import java.text.MessageFormat;
 
 import org.apache.commons.io.IOUtils;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClientBuilder;
 
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import com.kms.katalon.core.appium.constants.AppiumStringConstants;
+import com.kms.katalon.core.appium.constants.CoreAppiumMessageConstants;
+import com.kms.katalon.core.configuration.RunConfiguration;
 import com.kms.katalon.core.constants.StringConstants;
 import com.kms.katalon.core.logging.KeywordLogger;
+import com.kms.katalon.core.network.HttpClientProxyBuilder;
+import com.kms.katalon.core.network.ProxyInformation;
 
 public class AppiumRequestService {
     
@@ -27,16 +32,18 @@ public class AppiumRequestService {
         this.appiumServerUrl = appiumServerUrl;
     }
 
-    private String sendGetRequest(String url) throws UnsupportedOperationException, IOException {
-        try (CloseableHttpClient client = HttpClientBuilder.create().build();
+    private String sendGetRequest(String url) throws UnsupportedOperationException, IOException, URISyntaxException, GeneralSecurityException {
+        ProxyInformation proxyInfo = RunConfiguration.getProxyInformation();
+        try (CloseableHttpClient client = HttpClientProxyBuilder.create(proxyInfo).getClientBuilder().build();
                 CloseableHttpResponse response = client.execute(new HttpGet(url))) {
             return IOUtils.toString(response.getEntity().getContent(), StringConstants.DF_CHARSET);
         }
     }
 
     public void logAppiumInfo() {
+        String appiumJsonResponseStatus = "";
         try {
-            String appiumJsonResponseStatus = sendGetRequest(appiumServerUrl + APPIUM_URL_STATUS_PATH);
+            appiumJsonResponseStatus = sendGetRequest(appiumServerUrl + APPIUM_URL_STATUS_PATH);
 
             JsonObject parser = new JsonParser().parse(appiumJsonResponseStatus).getAsJsonObject();
             String appiumVersion = parser.getAsJsonObject("value")
@@ -45,9 +52,12 @@ public class AppiumRequestService {
                     .getAsString();
 
             logger.logRunData(AppiumStringConstants.XML_LOG_APPIUM_VERSION, appiumVersion);
-        } catch (UnsupportedOperationException | IOException e) {
+            logger.logRunData(CoreAppiumMessageConstants.XML_LOG_APPIUM_STATUS, parser.toString());
+        } catch (IllegalStateException e) {
+            logger.logRunData("appiumStatus", appiumJsonResponseStatus);
+        } catch (Exception e) {
             logger.logWarning(MessageFormat.format(AppiumStringConstants.MSG_UNABLE_TO_GET_APPIUM_STATUS,
-                    e.getMessage()));
+                    e.getMessage()), null, e);
         }
     }
 }
--- Include/scripts/groovy/com/kms/katalon/core/appium/driver/SwipeableAndroidDriver.java
+++ Include/scripts/groovy/com/kms/katalon/core/appium/driver/SwipeableAndroidDriver.java
@@ -1,20 +1,27 @@
 package com.kms.katalon.core.appium.driver;
 
-import io.appium.java_client.android.AndroidDriver;
-
 import java.net.URL;
 
 import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.interactions.HasTouchScreen;
 import org.openqa.selenium.interactions.TouchScreen;
+import org.openqa.selenium.remote.CommandExecutor;
+import org.openqa.selenium.remote.HttpCommandExecutor;
 import org.openqa.selenium.remote.RemoteTouchScreen;
 
-@SuppressWarnings({ "unchecked", "rawtypes" })
+import io.appium.java_client.android.AndroidDriver;
+
+@SuppressWarnings({ "rawtypes" })
 public class SwipeableAndroidDriver extends AndroidDriver implements HasTouchScreen {
     private RemoteTouchScreen touch;
 
     public SwipeableAndroidDriver(URL remoteAddress, Capabilities desiredCapabilities) {
         super(remoteAddress, desiredCapabilities);
+        touch = new RemoteTouchScreen(getExecuteMethod());
+    }
+    
+    public SwipeableAndroidDriver(HttpCommandExecutor executor, Capabilities desiredCapabilities) {
+        super(executor, desiredCapabilities);
         touch = new RemoteTouchScreen(getExecuteMethod());
     }
 
